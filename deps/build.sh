ee31d3290b1291e49dff92200f29f1b42b156839
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2021-03-13 23:56:26 +0100

    Improve build entrypoint script
    Allow to pass extra arguments to cmake and make
    
    Change-Id: I860934d145949acf685fd10e5693ebc94dbf9717

--- deps/build.sh
+++ deps/build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 # Prepend variables MAKE_PROCS ('make' parallel jobs, maximum available by default) and BUILD_TYPE ([Release]|Debug).
+# Command-line arguments:
+# * Having cmake system:
+#   $1: extra cmake arguments; $2-$@: extra make arguments
+# * Not having cmake system:
+#   $@: extra make arguments
+
 if [ -x ./`basename $0` ]
 then
    exec ./`basename $0` $@
@@ -7,8 +13,8 @@ else
    BUILD_TYPE=${BUILD_TYPE:-Release}
    MAKE_PROCS=${MAKE_PROCS:-$(grep processor /proc/cpuinfo -c)}
 
-   [ -f CMakeLists.txt ] && cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} $@ .
-   make -j${MAKE_PROCS}
+   [ -f CMakeLists.txt ] && cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} $1 . && shift
+   make -j${MAKE_PROCS} $@
 fi
 exit $?
 
