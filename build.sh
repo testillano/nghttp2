334900af1deec18d516979f4c9692bb549694abc
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2021-03-31 03:46:03 +0200

    Improve build script

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 #############
 # VARIABLES #
 #############
+FORCE_DEFAULTS=
+image_tag__dflt=latest
 make_procs__dflt=$(grep processor /proc/cpuinfo -c)
 base_tag__dflt=latest # alpine
 nghttp2_ver__dflt=1.42.0 # tatsuhiro
@@ -11,30 +13,44 @@ boost_ver__dflt=1.67.0 # boost
 #############
 # FUNCTIONS #
 #############
-# $1: question; $2: variable by reference; $3: default value
+# $1: variable by reference; $2: default value
 _read() {
-  local question=$1
-  local -n varname=$2
-  local default=$3
+  local -n varname=$1
+  local default=$2
 
-  echo "${question} [${default}]:"
-  read varname
-  [ -z "${varname}" ] && varname=${default}
+  local s_default="<null>"
+  [ -n "${default}" ] && s_default="${default}"
+  echo "Input '$1' value [${s_default}]:"
+
+  [ -n "${FORCE_DEFAULTS}" ] && varname=${default}
+
+  if [ -n "${varname}" ]
+  then
+    echo "${varname}"
+  else
+    read varname
+    [ -z "${varname}" ] && varname=${default}
+  fi
 }
 
 #############
 # EXECUTION #
 #############
 cd $(dirname $0)
+[ "$1" = "-f" ] && FORCE_DEFAULTS=yes
 echo
-echo "-------------------"
-echo "Build nghttp2 image"
-echo "-------------------"
+echo "=== Build nghttp2 image ==="
 echo
-_read "Input 'make_procs'" make_procs ${make_procs__dflt}
-_read "Input alpine 'base_tag'" base_tag ${base_tag__dflt}
-_read "Input tatsuhiro 'nghttp2_ver'" nghttp2_ver ${nghttp2_ver__dflt}
-_read "Input boost 'boost_ver'" boost_ver ${boost_ver__dflt}
+echo "For headless mode, prepend/export asked variables:"
+echo " $(grep "^_read " build.sh | awk '{ print $2 }' | tr '\n' ' ')"
+echo
+echo "Provide '-f' to force defaults."
+echo
+_read image_tag ${image_tag__dflt}
+_read make_procs ${make_procs__dflt}
+_read base_tag ${base_tag__dflt}
+_read nghttp2_ver ${nghttp2_ver__dflt}
+_read boost_ver ${boost_ver__dflt}
 
 bargs="--build-arg make_procs=${make_procs}"
 bargs+=" --build-arg base_tag=${base_tag}"
@@ -42,6 +58,6 @@ bargs+=" --build-arg nghttp2_ver=${nghttp2_ver}"
 bargs+=" --build-arg boost_ver=${boost_ver}"
 
 set -x
-docker build --rm ${bargs} -t testillano/nghttp2 .
+docker build --rm ${bargs} -t testillano/nghttp2:${image_tag} .
 set +x
 
